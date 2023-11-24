64e67a1013857ae27fed361e77b90fe5a2bde813
Jesse Granier <jesse.granier@lapiscine.pro>
2023-11-24 16:26:11 +0100

    Use Effect, ou pas

--- src/pages/RandomCocktails.jsx
+++ src/pages/RandomCocktails.jsx
@@ -1,30 +1,33 @@
 import { useState } from "react"
+import { useEffect } from "react";
 
 function RandomCocktailPage(){
 
     const [randomCocktail, setRandomCocktail] = useState(null)
     // on met le useState pour garder en mémoire la modification.
     // un if pour lancer le fetch qui va récupérer les infos, transformer en json puis lisible javascript
-    // setRandomCocktails pour modifier la variable randomCocktails (car on est sur un UseState)
+    // setRandomCocktails pour modifier la variable randomCocktails (car on est sur un UseState) et recharger le composant.
     // Partie JSX, on crée la partie "avant la requête" et "après la requête".
-    if(randomCocktail === null){
-        (async()=>{
-            const cocktailRandomResponse = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-            const cocktailRandomInJs = await cocktailRandomResponse.json()
-            console.log(cocktailRandomInJs);
-            setRandomCocktail(cocktailRandomInJs.drinks)
-        })()
-    }
-    console.log(randomCocktail);
+
+    // UseEffect sert à faire fonctionner le composant une seule fois via le tableau vide sur la fin [].
+    // On encadre le async par le UseEffect pour que ce composant ne soit lancé qu'une seule fois.
+    useEffect(() => {
+        (async () => {
+          const cocktailRandomResponse = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+          const cocktailJs = await cocktailRandomResponse.json();
+    
+          setRandomCocktail(cocktailJs.drinks[0]);
+        })();
+      }, []);
 
     return(
         <>
-            {randomCocktail? (
+            {randomCocktail !== null ? (
                 <main>
                     <p>Là, ca va donner. Regarde donc ça :</p>
-                    <h3>{randomCocktail[0].strDrink}</h3>
-                    <img src={randomCocktail[0].strDrinkThumb} alt={randomCocktail[0].strDrink} />
-                    <p>{randomCocktail[0].strInstructions}</p>
+                    <h3>{randomCocktail.strDrink}</h3>
+                    <img src={randomCocktail.strDrinkThumb} alt={randomCocktail.strDrink} />
+                    <p>{randomCocktail.strInstructions}</p>
                     <p>ouais en anglais. T'as qu'à apprendre.</p>
                 </main>
             ):(

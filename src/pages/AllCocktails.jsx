c199295940058691a67de29d59bfd0c4bd6818dc
Jesse Granier <jesse.granier@lapiscine.pro>
2023-11-24 11:10:56 +0100

    fonction anonyme auto-invoquée

--- src/pages/AllCocktails.jsx
+++ src/pages/AllCocktails.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
 
+
+// Pour la fonction anonyme avec async : la fonction va exécuter le même code que le précédent mais sans les .then (on en avait 2, on peut en avoir
+// beaucoup plus). De plus la lecture est plus claire et le code simplifié.
+// On se sert d'une fonction anonyme qui s'auto-invoque : Elle reste anonyme car aucun nom et s'invoque avec les () de la fin.
+// On aurait pu l'appeler mais préciser du coup async, ce qui est tiré par les cheveux que je n'ai plus.
+
+
 function AllCocktails() {
 
     const [cocktails, setCocktails] = useState(null);
 
-    if (!cocktails) {
-        fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
-            .then((response) => response.json())
-            .then((json) => {
-                console.log(json);
-                setCocktails(json.drinks);
-            });
+    if (!cocktails) {        
+        (async () => {
+            const cocktailsResponse = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=");
+            const cocktailsInJs = await cocktailsResponse.json();
+            setCocktails(cocktailsInJs.drinks);
+
+        })();
     }
     return (
         <main>
